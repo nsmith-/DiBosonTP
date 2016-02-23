d85266d9b5c3acc37b01c725d71a6c696cdd1c90
Nick Smith <nick.smith@cern.ch>
2016-02-23 06:31:03 -0600

    More work on new fits

--- electrons/newfitter.py
+++ electrons/newfitter.py
@@ -4,19 +4,46 @@ ROOT.gROOT.SetBatch(True)
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 from Analysis.DiBosonTP.PassFailSimulFitter import PassFailSimulFitter
 
+pdfDefData = [
+        'RooBreitWigner::signalPhy(mass, 91, 2.5)',
+        'Gaussian::signalResPass(mass,meanSmearing[1.0,-1.0,2.0],sigmaSmearing[0.5,0.07,3.0])',
+        'Gaussian::signalResFail(mass,meanSmearing              ,sigmaSmearing              )',
+        'RooBernstein::backgroundPass(mass, {a0[10,0,50],a1[1,0,50],a2[1,0,50],a3[1,0,50]})',
+        'RooBernstein::backgroundFail(mass, {b0[10,0,50],b1[1,0,50],b2[1,0,50],b3[1,0,50]})',
+        'FCONV::signalPass(mass, signalPhy, signalResPass)',
+        'FCONV::signalFail(mass, signalPhy, signalResFail)',
+        'efficiency[0.9,0,1]',
+        "expr::numSignalPass('efficiency*numSignalAll', efficiency, numSignalAll[10.,10000000000])",
+        "expr::numSignalFail('(1-efficiency)*numSignalAll', efficiency, numSignalAll)",
+        "SUM::pdfPass(numSignalPass*signalPass, numBackgroundPass[0.,1000000000]*backgroundPass)",
+        "SUM::pdfFail(numSignalFail*signalFail, numBackgroundFail[0.,1000000000]*backgroundFail)",
+        "SIMUL::simPdf(decision, Passed=pdfPass, Failed=pdfFail)",
+        ]
 
 
 def main() :
     # DEMO
     pdfDef = [
             'RooBreitWigner::signalPhy(mass, 91, 2.5)',
-            'Gaussian::signalResPass(mass,meanSmearing[1.0,-1.0,2.0],sigmaSmearing[0.5,0.07,3.0])',
-            'Gaussian::signalResFail(mass,meanSmearing              ,sigmaSmearing              )',
-            'RooBernstein::backgroundPass(mass, {a0[10,0,50],a1[1,0,50],a2[1,0,50],a3[1,0,50]})',
-            'RooBernstein::backgroundFail(mass, {b0[10,0,50],b1[1,0,50],b2[1,0,50],b3[1,0,50]})',
+            #'Gaussian::signalResPass(mass,meanSmearing[1.0,-1.0,2.0],sigmaSmearing[0.5,0.07,3.0])',
+            #'Gaussian::signalResFail(mass,meanSmearing              ,sigmaSmearing              )',
+            "RooCBExGaussShape::signalResPass(mass,meanP[-0.0,-5.000,5.000],sigmaP[0.956,0.00,15.000],alphaP[0.999, 0.0,50.0],nP[1.405,0.000,50.000],sigmaP_2[1.000,0.500,15.00])",
+            "RooCBExGaussShape::signalResFail(mass,meanF[-0.0,-5.000,5.000],sigmaF[3.331,0.00,15.000],alphaF[1.586, 0.0,50.0],nF[0.464,0.000,20.00], sigmaF_2[1.675,0.500,12.000])",
             'FCONV::signalPass(mass, signalPhy, signalResPass)',
             'FCONV::signalFail(mass, signalPhy, signalResFail)',
+            'Gaussian::signalSideband(mass, sbMean[75,60,90], sbSigma[5,3,20])',
+            'sigSidebandFracPass[0.001,0,1]',
+            'sigSidebandFracFail[0.001,0,1]',
             'efficiency[0.9,0,1]',
+            "expr::numSignalPass('efficiency*numSignalAll', efficiency, numSignalAll[10.,10000000000])",
+            "expr::numSignalPassPeak('(1-sigSidebandFracPass)*numSignalPass', sigSidebandFracPass, numSignalPass)",
+            "expr::numSignalPassSB('sigSidebandFracPass*numSignalPass', sigSidebandFracPass, numSignalPass)",
+            "expr::numSignalFail('(1-efficiency)*numSignalAll', efficiency, numSignalAll)",
+            "expr::numSignalFailPeak('(1-sigSidebandFracFail)*numSignalFail', sigSidebandFracFail, numSignalFail)",
+            "expr::numSignalFailSB('sigSidebandFracFail*numSignalFail', sigSidebandFracFail, numSignalFail)",
+            'SUM::pdfPass(numSignalPassPeak*signalPass, numSignalPassSB*signalSideband)',
+            'SUM::pdfFail(numSignalFailPeak*signalFail, numSignalFailSB*signalSideband)',
+            "SIMUL::simPdf(decision, Passed=pdfPass, Failed=pdfFail)",
             ]
     fitVariable = ROOT.RooRealVar('mass', 'TP Pair Mass', 60, 120, 'GeV')
 
@@ -27,16 +54,19 @@ def main() :
     t = f.Get('GsfElectronToRECO/fitter_tree')
 
     def fit(name, allProbeCondition, passingProbeCondition) :
-        hpass = ROOT.TH1F('passed_probes', '', 60,60,120)
-        hfail = hpass.Clone('failed_probes')
-        t.Draw('mass >> passed_probes', '(%s)*(%s)' % (allProbeCondition, passingProbeCondition), 'goff')
-        t.Draw('mass >> failed_probes', '(%s)*!(%s)' % (allProbeCondition, passingProbeCondition), 'goff')
+        hpass = ROOT.TH1F(name+'_passed_probes', '', 60,60,120)
+        hfail = hpass.Clone(name+'_failed_probes')
+        if type(allProbeCondition) is list :
+            allProbeCondition = '&&'.join(allProbeCondition)
+        t.Draw('mass >> %s_passed_probes' % name, 'totWeight*(%s)*(%s)' % (allProbeCondition, passingProbeCondition), 'goff')
+        t.Draw('mass >> %s_failed_probes' % name, 'totWeight*(%s)*!(%s)' % (allProbeCondition, passingProbeCondition), 'goff')
         if hfail.Integral() < 1 :
             print 'No failed probes for cut:', '(%s)*!(%s)' % (allProbeCondition, passingProbeCondition)
             return
-        fitter = PassFailSimulFitter(name, pdfDef, fitVariable)
+        fitter = PassFailSimulFitter(name, fitVariable)
+        fitter.setPdf(pdfDef)
         fitter.setData(hpass, hfail)
-        res = fitter.fit()
+        res = fitter.fit('simPdf', 'data')
         c = fitter.drawFitCanvas()
         c.Print('plots/fit_%s.png' % name)
         fout = ROOT.TFile('plots/fit_%s.root' % name, 'recreate')
@@ -47,16 +77,19 @@ def main() :
         fitter.workspace.Write()
         fout.Close()
 
-    fit('ZZTight_pt7to15', 'probe_Ele_pt>7 && probe_Ele_pt<15', 'passingZZTight')
-    fit('ZZTight_pt15to30', 'probe_Ele_pt>15 && probe_Ele_pt<30', 'passingZZTight')
-    fit('ZZTight_pt30to50', 'probe_Ele_pt>30 && probe_Ele_pt<50', 'passingZZTight')
+    mcCond = ['mcTrue']
 
-    fit('ZZLoose_pt7to15', 'probe_Ele_pt>7 && probe_Ele_pt<15', 'passingZZLoose')
-    fit('ZZLoose_pt15to30', 'probe_Ele_pt>15 && probe_Ele_pt<30', 'passingZZLoose')
-    fit('ZZLoose_pt30to50', 'probe_Ele_pt>30 && probe_Ele_pt<50', 'passingZZLoose')
+    fit('ZZTight_pt7to15',  mcCond+['probe_Ele_pt>7 && probe_Ele_pt<15'], 'passingZZTight')
+    fit('ZZTight_pt15to30', mcCond+['probe_Ele_pt>15 && probe_Ele_pt<30'], 'passingZZTight')
+    fit('ZZTight_pt30to50', mcCond+['probe_Ele_pt>30 && probe_Ele_pt<50'], 'passingZZTight')
 
-#    from IPython import embed
-#    embed()
+    fit('ZZLoose_pt7to15',  mcCond+['probe_Ele_pt>7 && probe_Ele_pt<15'], 'passingZZLoose')
+    fit('ZZLoose_pt15to30', mcCond+['probe_Ele_pt>15 && probe_Ele_pt<30'], 'passingZZLoose')
+    fit('ZZLoose_pt30to50', mcCond+['probe_Ele_pt>30 && probe_Ele_pt<50'], 'passingZZLoose')
+
+    fit('ZZIso_pt7to15',  mcCond+['probe_Ele_pt>7 && probe_Ele_pt<15'],  'passingZZIso')
+    fit('ZZIso_pt15to30', mcCond+['probe_Ele_pt>15 && probe_Ele_pt<30'], 'passingZZIso')
+    fit('ZZIso_pt30to50', mcCond+['probe_Ele_pt>30 && probe_Ele_pt<50'], 'passingZZIso')
 
 if __name__ == '__main__' :
     main()
--- python/PassFailSimulFitter.py
+++ python/PassFailSimulFitter.py
@@ -4,14 +4,6 @@ ROOT.gROOT.SetBatch(True)
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
 class PassFailSimulFitter :
-    commonPdfDefinitions = [
-        "expr::numSignalPass('efficiency*numSignalAll', efficiency, numSignalAll[10.,10000000000])",
-        "expr::numSignalFail('(1-efficiency)*numSignalAll', efficiency, numSignalAll)",
-        "SUM::pdfPass(numSignalPass*signalPass, numBackgroundPass[0.,1000000000]*backgroundPass)",
-        "SUM::pdfFail(numSignalFail*signalFail, numBackgroundFail[0.,1000000000]*backgroundFail)",
-        "SIMUL::simPdf(decision, Passed=pdfPass, Failed=pdfFail)",
-    ]
-
     def _wsimport(self, *args) :
         # getattr since import is special in python
         # NB RooWorkspace clones object
@@ -20,12 +12,13 @@ class PassFailSimulFitter :
             args += (ROOT.RooCmdArg(),)
         return getattr(self.workspace, 'import')(*args)
 
-    def __init__(self, name, pdfDefinition, fitVariable) :
+    def __init__(self, name, fitVariable) :
         self.workspace = ROOT.RooWorkspace(name)
         self._wsimport(fitVariable)
         self._fitVar = self.workspace.var(fitVariable.GetName())
         self.workspace.factory("decision[Passed,Failed]")
-        pdfDefinition.extend(self.commonPdfDefinitions)
+
+    def setPdf(self, pdfDefinition) :
         for line in pdfDefinition :
             self.workspace.factory(line)
 
@@ -42,11 +35,11 @@ class PassFailSimulFitter :
         data = ROOT.RooDataHist('data', 'dataset', ROOT.RooArgList(self._fitVar), self.workspace.cat('decision'), m)
         self._wsimport(data)
 
-    def fit(self) :
+    def fit(self, pdfName, dataName) :
         w = self.workspace
         rf = ROOT.RooFit
-        pdf = w.pdf('simPdf')
-        data = w.data('data')
+        pdf = w.pdf(pdfName)
+        data = w.data(dataName)
 
         # Initialize some parameters
         signalFractionPassing = 0.9
@@ -55,8 +48,9 @@ class PassFailSimulFitter :
         initialEff = w.var('efficiency').getVal()
         nSignal = nPass*signalFractionPassing/initialEff
         w.var('numSignalAll').setVal(nSignal)
-        w.var('numBackgroundPass').setVal(nPass-nSignal*initialEff)
-        w.var('numBackgroundFail').setVal(nPass-max(nSignal*(1-initialEff), nFail))
+        if w.var('numBackgroundPass') :
+            w.var('numBackgroundPass').setVal(nPass-nSignal*initialEff)
+            w.var('numBackgroundFail').setVal(nPass-max(nSignal*(1-initialEff), nFail))
         w.saveSnapshot('preFit', w.components())
 
         minosVars = ROOT.RooArgSet(w.var('efficiency'))
@@ -71,7 +65,7 @@ class PassFailSimulFitter :
         w.saveSnapshot('postFit', w.components())
         return result
 
-    def drawFitCanvas(self) :
+    def drawFitCanvas(self, pdfName, dataName) :
         w = self.workspace
         fitVar = self._fitVar
         rf = ROOT.RooFit
@@ -80,22 +74,25 @@ class PassFailSimulFitter :
         failFrame = fitVar.frame(rf.Name('Failing'), rf.Title('Failing Probes'))
         allFrame  = fitVar.frame(rf.Name('All'),     rf.Title('All Probes'))
 
-        pdf = w.pdf('simPdf')
-        data = w.data('data')
+        pdf = w.pdf(pdfName)
+        data = w.data(dataName)
         datapass = data.reduce(rf.Cut('decision==decision::Passed'))
         datafail = data.reduce(rf.Cut('decision==decision::Failed'))
 
         datapass.plotOn(passFrame)
         pdf.plotOn(passFrame, rf.Slice(w.cat('decision'), 'Passed'), rf.ProjWData(datapass), rf.LineColor(ROOT.kGreen))
-        pdf.plotOn(passFrame, rf.Slice(w.cat('decision'), 'Passed'), rf.ProjWData(datapass), rf.LineColor(ROOT.kGreen), rf.Components('backgroundPass'), rf.LineStyle(ROOT.kDashed))
+        if w.pdf('backgroundPass') :
+            pdf.plotOn(passFrame, rf.Slice(w.cat('decision'), 'Passed'), rf.ProjWData(datapass), rf.LineColor(ROOT.kGreen), rf.Components('backgroundPass'), rf.LineStyle(ROOT.kDashed))
 
         datafail.plotOn(failFrame)
         pdf.plotOn(failFrame, rf.Slice(w.cat('decision'), 'Failed'), rf.ProjWData(datafail), rf.LineColor(ROOT.kRed))
-        pdf.plotOn(failFrame, rf.Slice(w.cat('decision'), 'Failed'), rf.ProjWData(datafail), rf.LineColor(ROOT.kRed), rf.Components('backgroundFail'), rf.LineStyle(ROOT.kDashed))
+        if w.pdf('backgroundFail') :
+            pdf.plotOn(failFrame, rf.Slice(w.cat('decision'), 'Failed'), rf.ProjWData(datafail), rf.LineColor(ROOT.kRed), rf.Components('backgroundFail'), rf.LineStyle(ROOT.kDashed))
 
         data.plotOn(allFrame)
         pdf.plotOn(allFrame, rf.ProjWData(data))
-        pdf.plotOn(allFrame, rf.ProjWData(data), rf.LineColor(ROOT.kBlue), rf.Components('backgroundPass,backgroundFail'), rf.LineStyle(ROOT.kDashed))
+        if w.pdf('backgroundPass') :
+            pdf.plotOn(allFrame, rf.ProjWData(data), rf.LineColor(ROOT.kBlue), rf.Components('backgroundPass,backgroundFail'), rf.LineStyle(ROOT.kDashed))
 
         # infoFrame is a placeholder
         infoFrame = fitVar.frame(rf.Name("Fit Results"), rf.Title("Fit Results"))
@@ -118,58 +115,3 @@ class PassFailSimulFitter :
         c.GetListOfPrimitives().SetOwner(True)
         return c
 
-
-def main() :
-    # DEMO
-    pdfDef = [
-            'RooBreitWigner::signalPhy(mass, 91, 2.5)',
-            'Gaussian::signalResPass(mass,meanSmearing[1.0,-1.0,2.0],sigmaSmearing[0.5,0.07,3.0])',
-            'Gaussian::signalResFail(mass,meanSmearing              ,sigmaSmearing              )',
-            'RooBernstein::backgroundPass(mass, {a0[10,0,50],a1[1,0,50],a2[1,0,50],a3[1,0,50]})',
-            'RooBernstein::backgroundFail(mass, {b0[10,0,50],b1[1,0,50],b2[1,0,50],b3[1,0,50]})',
-            'FCONV::signalPass(mass, signalPhy, signalResPass)',
-            'FCONV::signalFail(mass, signalPhy, signalResFail)',
-            'efficiency[0.9,0,1]',
-            ]
-    fitVariable = ROOT.RooRealVar('mass', 'TP Pair Mass', 60, 120, 'GeV')
-
-    ROOT.RooMsgService.instance().setGlobalKillBelow(ROOT.RooFit.ERROR)
-    ROOT.gStyle.SetOptTitle(True)
-
-    # Make up some data
-    hpass = ROOT.TH1F('passed', '', 60,60,120)
-    hfail = hpass.Clone('failed')
-    bw = ROOT.TF1('fbw', 'TMath::Voigt(x-91, 2, 2.5)', 60, 120)
-    bkg = ROOT.TF1('fbkg', 'exp(-(x-60)/30)', 60, 120)
-    hpass.FillRandom('fbw', 3000)
-    hpass.FillRandom('fbkg', 80)
-    hfail.FillRandom('fbw', 100)
-    hfail.FillRandom('fbkg', 75)
-
-    fitter = PassFailSimulFitter('demoFit', pdfDef, fitVariable)
-    fitter.setData(hpass, hfail)
-    res = fitter.fit()
-    c = fitter.drawFitCanvas()
-
-    f = ROOT.TFile('fit.root', 'recreate')
-    d = f.mkdir('fit_output')
-    d.cd()
-    c.Write()
-    res.Write()
-    fitter.workspace.Write()
-
-    fitter = PassFailSimulFitter('demoFit2', pdfDef, fitVariable)
-    fitter.setData(hpass, hfail)
-    res = fitter.fit()
-    c = fitter.drawFitCanvas()
-
-    d = f.mkdir('fit_output_2')
-    d.cd()
-    c.Write()
-    res.Write()
-    fitter.workspace.Write()
-#    from IPython import embed
-#    embed()
-
-if __name__ == '__main__' :
-    main()
